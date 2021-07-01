4bd2f9eb5c58a51bbc8cacfba751bdd47c5db861
claudiopedalino <claudio.dpedalino@gmail.com>
2021-06-30 23:41:49 -0300

    testing pre push

--- ChallengeNubimetrics.Application/Services/QueueService.cs
+++ ChallengeNubimetrics.Application/Services/QueueService.cs
@@ -15,57 +15,57 @@ namespace ChallengeNubimetrics.Application.Services
         public QueueService(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
 
+        public Task Produce<T>(T message, string queueName)
+        {
+            SetConnection(out IConnection connection, out IModel channel);
 
-            public Task Produce<T>(T message, string queueName)
+            channel.QueueDeclare(queueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("", queueName, null, body);
+
+            return Task.CompletedTask;
+        }
+
+        private void SetConnection(out IConnection connection, out IModel channel)
+        {
+            var factory = new ConnectionFactory()
             {
-                SetConnection(out IConnection connection, out IModel channel);
+                Uri = new Uri(_configuration.GetValue<string>("RabbitMQ:Hostname"))
+            };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+        }
 
-                channel.QueueDeclare(queueName,
-                                     durable: true,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+        public async Task Consume(string queueName)
+        {
+            SetConnection(out IConnection connection, out IModel channel);
 
-                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
-                channel.BasicPublish("", queueName, null, body);
+            channel.QueueDeclare(queueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
 
-                return Task.CompletedTask;
-            }
+            var consumer = new EventingBasicConsumer(channel);
 
-            private void SetConnection(out IConnection connection, out IModel channel)
+            consumer.Received += (sender, e) =>
             {
-                var factory = new ConnectionFactory()
-                {
-                    Uri = new Uri(_configuration.GetValue<string>("RabbitMQ:Hostname"))
-                };
-                connection = factory.CreateConnection();
-                channel = connection.CreateModel();
-            }
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
 
-            public async Task Consume(string queueName)
-            {
-                SetConnection(out IConnection connection, out IModel channel);
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            };
 
-                channel.QueueDeclare(queueName,
-                                     durable: true,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-
-                consumer.Received += (sender, e) =>
-                {
-                    var body = e.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-
-                    Console.BackgroundColor = ConsoleColor.DarkYellow;
-                    Console.WriteLine(message);
-                    Console.ResetColor();
-                };
-
-                channel.BasicConsume(queueName, true, consumer);
-            }
+            channel.BasicConsume(queueName, true, consumer);
         }
     }
+}
